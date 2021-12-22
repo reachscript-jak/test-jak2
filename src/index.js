457728a8b111d506c99a4e3db90fb569b5985c13
Takumi Okada <44320334+reachscript-jak@users.noreply.github.com>
2021-12-22 20:47:48 +0900

    なんか対応した

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello じゃけぇです!</h1>
+<h1>Hello じゃけぇです!更新しました！！</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
